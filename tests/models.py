70fa3f5fd1b83482256679a0579eb07d9b92c75c
Daniël Franke <daniel@ams-sec.org>
2014-05-11 12:19:58 +0200

    Add an abstract model.

--- tests/models.py
+++ tests/models.py
@@ -7,7 +7,22 @@ class Simple(models.Model):
     """
 
     one = models.IntegerField()
-    two = models.CharField(max_length=20)
+    two = models.CharField(max_length=20, default='spam')
 
     def __unicode__(self):
         return '{}/{}'.format(self.one, self.two)
+
+
+class Abstract(models.Model):
+    """
+    Abstract test model.
+    """
+
+    one = models.IntegerField()
+    two = models.CharField(max_length=20, default='eggs')
+
+    def __unicode__(self):
+        return '{}/{}'.format(self.one, self.two)
+
+    class Meta:
+        abstract = True
